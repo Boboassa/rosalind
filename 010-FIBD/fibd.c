58584d6cce29705666562b199bcc35a238fde540
Boboassa <uca.baldassi99@gmail.com>
2021-09-17 00:00:12 +0200

    Add non functional code for problem 10 "FIBD"

--- 010-FIBD/fibd.c
+++ 010-FIBD/fibd.c
@@ -9,52 +9,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_GEN 100
 
 /* gigahyper big numbers */
-unsigned long long int rabbits ( int n, int k, int m, int death);
+//unsigned long long int rabbits ( int n, int k, int m, int death);
 
 int main( int argc, char *argv[])
 {
     int n = atoi(argv[1]);
-    int k = atoi(argv[2]);
-    int m = atoi(argv[3]);
+    int m = atoi(argv[2]);
+    unsigned long long int rabbits[MAX_GEN];
+    rabbits[0] = 1;
+    rabbits[1] = 1;
 
     /* If the wrong number of argument is present exit */
-    if ( argc != 4 )
+    if ( argc != 3 )
     {
         printf("Usage %s <n> <k> <m>\n", argv[0] );
         exit(1);
     }
 
+    for (int i = 2; i < n; i++) {
+        if ( (i-(m+1)) >= 0) {
+            rabbits[i] = rabbits[i-1] + rabbits[i-2] - rabbits[(i-(m+1))];
+        } else if ( (i-(m+1)) == -3 || (i-(m+1)) == -1) {
+            rabbits[i] = rabbits[i-1] + rabbits[i-2] - 1;
+        } else {
+            rabbits[i] = rabbits[i-1] + rabbits[i-2];
+        }
+    }
+
     /* Print the result */
-    printf("rabbits at generation %i: %llu\n", n, rabbits( n, k, m, 0));
+    for (int i=0; i<n; i++) {
+    printf("rabbits at generation %i: %llu\n", i+1, rabbits[i]);
+    }
+    //printf("rabbits at generation %i: %llu\n", n, rabbits[n-1]);
     return 0;
+
 }
 
-unsigned long long int rabbits ( int n, int k, int m, int death)
-{
-    /* Recursion */
-    if ( n > 2 ) {
-        unsigned long long int prevGen;
-        unsigned long long int prevPrevGen;
-        unsigned long long int deathGen;
-
-        prevGen = rabbits(n-1, k, m, 0);
-        prevPrevGen = rabbits(n-2, k, m, 0);
-        deathGen = rabbits(n-(m+1), k, m, 1);
-
-        //return(prevGen + k * prevPrevGen - deathGen);
-        return(prevGen + prevPrevGen - deathGen);
-    }
-    else if ( n == 1 || n == 2 ) {
-        return(1);
-    }
-    /* we gotta go deeper than the generation 1 to find the first couple *
-     * dead rabbits                                                      */
-    else if ( (n == 0 || n == -2) && death ) {
-        return(1);
-    }
-    else {
-        return (0);
-    }
-}
